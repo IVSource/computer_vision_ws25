79fc43aecaf12310af1f9b671bb2bb7a7648308d
Ilja Selenzow <ilja.selenzow@rwu.de>
2025-11-24 19:45:32 +0100

    Logic to hang a poser on the wall is done, some cleanup and a GUI would be nice

--- wall_poster.py
+++ wall_poster.py
@@ -1,15 +1,21 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 
 
 def main():
     upper = 200
     lower = 100
     ESC_KEY = 27
-    img = cv.imread('./data/classroom wall/20221115_113319.jpg',
-                    cv.IMREAD_GRAYSCALE)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    # wall_grey = cv.imread('./data/classroom wall/20221115_113340.jpg', cv.IMREAD_GRAYSCALE)
+    wall_grey = cv.imread('./data/classroom wall/20221115_113346.jpg', cv.IMREAD_GRAYSCALE)
+    assert wall_grey is not None, "WALL file could not be read, check with os.path.exists()"
+
+    # wall_color = cv.imread('./data/classroom wall/20221115_113340.jpg', cv.IMREAD_COLOR)
+    wall_color = cv.imread('./data/classroom wall/20221115_113346.jpg', cv.IMREAD_COLOR)
+    assert wall_color is not None, "WALL file could not be read, check with os.path.exists()"
+
+    poster = cv.imread('./starry_night.jpg', cv.IMREAD_COLOR)
+    assert poster is not None, "POSTER file could not be read, check with os.path.exists()"
 
     # Create a black image, a window
     # cv.namedWindow('image', cv.WINDOW_NORMAL)
@@ -20,13 +26,25 @@ def main():
     detector = cv.aruco.ArucoDetector(aruco_dict, parameters)
 
     # Detect markers
-    corners, ids, rejected = detector.detectMarkers(img)
-    color_img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    corners, ids, rejected = detector.detectMarkers(wall_grey)
+    print('Detected corners: ', corners)
+
+    source_points = np.zeros((4, 2), dtype=np.float32)
+    print('Original corners: ', source_points)
+    source_points[0] = [0, 0]
+    source_points[1] = [100, 0]
+    source_points[2] = [100, 100]
+    source_points[3] = [0, 100]
+    print(source_points)
+    target_points = np.array(corners).reshape((4, 2)).astype(np.float32)
+
+    projection_matrix = cv.getPerspectiveTransform(source_points, target_points)
+    print(projection_matrix)
 
     # Draw detected markers
     if ids is not None:
-        color_img = cv.aruco.drawDetectedMarkers(
-            color_img, corners, ids, borderColor=(0, 255, 0))
+        wall_color = cv.aruco.drawDetectedMarkers(
+            wall_color, corners, ids, borderColor=(0, 255, 0))
 
         # Print marker information
         for i, marker_id in enumerate(ids.flatten()):
@@ -36,10 +54,20 @@ def main():
             print(
                 f"Marker ID: {marker_id}, Center: ({center_x:.2f}, {center_y:.2f})")
 
+    projection_matrix @= np.array([[1, 0, -100], [0, 1, -300], [0, 0, 1]])
+
+    projection_pane = np.zeros((*wall_grey.shape, 3), dtype=np.uint8)
+    cv.warpPerspective(poster, projection_matrix,
+                       (wall_grey.shape[1], wall_grey.shape[0]), wall_color, borderMode=cv.BORDER_TRANSPARENT)
+
     cv.namedWindow('ArUco Detection', cv.WINDOW_NORMAL)
-    cv.imshow('ArUco Detection', color_img)
+    # cv.namedWindow('Poster Projection', cv.WINDOW_NORMAL)
+    cv.imshow('ArUco Detection', wall_color)
+    # cv.imshow('Poster Projection', projection_pane)
 
     key = cv.waitKey(0)
+
+    cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
